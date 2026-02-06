69b5fd30cd5cb436b1cfc2b9a077a4b0c09ac6de
grrru <gruns0989@naver.com>
2026-02-06 16:56:09 +0900

    colorscheme은 lazy.lua에서 설치

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,13 +5,6 @@ return {
       -- colorscheme = "tokyonight-night",
       colorscheme = "catppuccin-latte",
       -- colorscheme = "grrruterm2",
-      -- colorscheme = "sonokai",
-      -- colorscheme = "github_dark_dimmed",
-      -- colorscheme = "github_dark_default"
     },
   },
-
-  require("plugins.themes.catppuccin"),
-  require("plugins.themes.sonokai"),
-  require("plugins.themes.github"),
 }
--- lua/plugins/themes/github.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    config = function()
-      require("github-theme").setup({})
-    end,
-  },
-}
