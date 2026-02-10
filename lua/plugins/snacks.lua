f9729f8aa528d52bb56238965b9c660417550061
grrru <gruns0989@naver.com>
2026-02-10 10:38:49 +0900

    snacks explorer,picker 설정

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = false,
+        },
+        files = {
+          hidden = true,
+          ignored = false,
+        },
+      },
+    },
+  },
+}
