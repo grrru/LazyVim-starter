9f9580c4716d917f0232750241369cba0e196b22
grrru <gruns0989@gmail.com>
2026-03-04 15:21:29 +0900

    snacks explorer의 diagnostic 아이콘을 error만 보이도록 설정

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,6 +6,13 @@ return {
         explorer = {
           hidden = true,
           ignored = false,
+          layout = { layout = { width = 30 } },
+          format = function(item, picker)
+            if item.severity and item.severity > vim.diagnostic.severity.ERROR then
+              item.severity = nil
+            end
+            return Snacks.picker.format.file(item, picker)
+          end,
         },
         files = {
           hidden = true,
